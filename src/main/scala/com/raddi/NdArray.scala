1dcc13e6df4a911edc76dedf931ec6779e690a8d
NetradeepakC <netradeepakc@gmail.com>
2023-04-25 00:57:10 +0530

    Added Basic Array Functions

--- src/main/scala/com/raddi/NdArray.scala
+++ src/main/scala/com/raddi/NdArray.scala
@@ -17,7 +17,7 @@ object NdArray {
   def make2dArray[A](dataframe: Dataframe, dataType: String): NArray[A] = {
     val filtered = dataframe.data.map(row => {
       row.zip(dataframe.columns.map(tuple => tuple._2)).filter(element => {
-         if(Type.getTypePrecedence(dataType, element._2)) true else false
+        if(Type.getTypePrecedence(dataType, element._2)) true else false
       }).map(pair => pair._1)
     })
     val data = filtered.map(row => row.map {
@@ -34,6 +34,49 @@ object NdArray {
     else {
       Some(left.data.zip(right.data).map(pair => pair._1 * pair._2).sum)
     }
+  }
+
+  def *[A: Numeric](left: A, right: Array1[A]): Array1[A] = {
+    def  mult[A: Numeric](left: A, right: Vector[A]): Vector[A] = {
+      if(right.length>1){
+        (left * right.head) +: mult(left, right.tail)
+      }
+      else{
+        Vector(left * right.head)
+      }
+    }
+    Array1(mult(left,right.data))
+  }
+
+  def +[A: Numeric](left: Array1[A], right: Array1[A]): Option[Array1[A]] = {
+    if(left.data.length!=right.data.length){
+      None
+    }
+    else{
+      Some(Array1(left.data.zip(right.data).map(pair => pair._1 + pair._2)))
+    }
+  }
+
+  def -[A: Numeric](left: Array1[A], right: Array1[A]): Option[Array1[A]] = {
+    if (left.data.length != right.data.length) {
+      None
+    }
+    else {
+      Some(Array1(left.data.zip(right.data).map(pair => pair._1 - pair._2)))
+    }
+  }
+
+  def randArr(length: Int, max:Double): Array1[Double]= {
+    val r = scala.util.Random
+    def loop(length: Int, max: Double): Vector[Double] = {
+      if (length == 1) {
+        Vector(max * r.nextDouble)
+      }
+      else {
+        (max * r.nextDouble) +: loop(length - 1, max)
+      }
+    }
+    Array1(loop(length, max))
   }
 
   def transpose()
